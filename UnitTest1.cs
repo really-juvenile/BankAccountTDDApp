035f1a5129a81b637b92c8b44836bf58212595cc
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-08-24 12:17:26 +0530

    Eliminated magic number

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,67 +6,87 @@ namespace BankAccountTDDApp
         [TestMethod]
         public void TestIfDepositingPositiveAmount()
         {
+            var depositAmount = 500;
+            var expectedBalance = 500;
+
             BankAccount account = new BankAccount();
-            account.Deposit(500);
-            Assert.AreEqual(500,account.GetBalance());
+            account.Deposit(depositAmount);
+            Assert.AreEqual(expectedBalance, account.GetBalance());
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "Deposit Amount must be positive")]
         public void TestIfDepositingNegativeAmount()
         {
+            var depositAmount = -500;
+
             BankAccount account = new BankAccount();
-            account.Deposit(-500);
+            account.Deposit(depositAmount);
         }
 
         [TestMethod]
-        
         public void TestValidWithdrawalAmount_Positive()
         {
-            BankAccount account = new BankAccount();
-            account.Deposit(1000);
-            account.Withdraw(200);
-            Assert.AreEqual(800, account.GetBalance());
-        }
+            var initialDeposit = 1000;
+            var withdrawalAmount = 200;
+            var expectedBalance = 800;
 
+            BankAccount account = new BankAccount();
+            account.Deposit(initialDeposit);
+            account.Withdraw(withdrawalAmount);
+            Assert.AreEqual(expectedBalance, account.GetBalance());
+        }
 
         [TestMethod]
         [ExpectedException(typeof(InvalidOperationException), "Insufficient Funds")]
         public void TestWithdrawingMoreThanBalanceAmount()
         {
+            var initialDeposit = 500;
+            var withdrawalAmount = 50000;
+
             BankAccount account = new BankAccount();
-            account.Deposit(500);
-            account.Withdraw(50000);
+            account.Deposit(initialDeposit);
+            account.Withdraw(withdrawalAmount);
         }
 
         [TestMethod]
-        [ExpectedException (typeof(ArgumentException), "Withdrawal amount must be positive")] 
+        [ExpectedException(typeof(ArgumentException), "Withdrawal amount must be positive")]
         public void TestWithdrawing_Negative_Amount()
         {
+            var withdrawalAmount = -500;
+
             BankAccount account = new BankAccount();
-            account.Withdraw(-500);
+            account.Withdraw(withdrawalAmount);
         }
+
         [TestMethod]
         public void TestingIfFundsAreGettingTransferred()
         {
+            var depositAmount = 500;
+            var transferAmount = 100;
+            var expectedAccount1Balance = 400;
+            var expectedAccount2Balance = 100;
+
             BankAccount account1 = new BankAccount();
             BankAccount account2 = new BankAccount();
 
-            account1.Deposit(500);
-            account1.Transfer(account2, 100);
+            account1.Deposit(depositAmount);
+            account1.Transfer(account2, transferAmount);
 
-            Assert.AreEqual(400, account1.GetBalance());
-            Assert.AreEqual(100, account2.GetBalance());
+            Assert.AreEqual(expectedAccount1Balance, account1.GetBalance());
+            Assert.AreEqual(expectedAccount2Balance, account2.GetBalance());
         }
 
         [TestMethod]
-        [ExpectedException (typeof(ArgumentNullException),"The Account doesnt Exists")]
+        [ExpectedException(typeof(ArgumentNullException), "The Account doesn't exist")]
         public void TestTransferringToAccount_Not_Exists()
         {
-            BankAccount account1 = new BankAccount();
-            account1.Deposit(1000);
-            account1.Transfer(null, 500);
-        }
+            var depositAmount = 1000;
+            var transferAmount = 500;
 
+            BankAccount account1 = new BankAccount();
+            account1.Deposit(depositAmount);
+            account1.Transfer(null, transferAmount);
+        }
     }
 }
